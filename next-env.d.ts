fa8d6d5a72f06446c036533b729b8d3adb555199
natureswaysoil <natureswaysoil@gmail.com>
2025-08-21 09:14:36 -0400

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,16 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+diff --git a/next-env.d.ts b/next-env.d.ts
+index 4f11a03dc6cc37f2b5105c08f2e7b24c603ab2f4..fd36f9494e2c202c823cdc0f25a6d2e48d352a2b 100644
+--- a/next-env.d.ts
++++ b/next-env.d.ts
+@@ -1,5 +1,6 @@
+ /// <reference types="next" />
+ /// <reference types="next/image-types/global" />
++/// <reference types="next/navigation-types/compat/navigation" />
+ 
+ // NOTE: This file should not be edited
+ // see https://nextjs.org/docs/basic-features/typescript for more information.
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
